6e075f9b061486c05b6c56bc90a5e1aff1086af7
rkb26 <cbsebiology4u@gmail.com>
2026-08-18 08:02:26 +0530

    Add new guidelines documents for SSD schools
    Added multiple guidelines documents related to sports activities, maintenance works, and school programs.

--- data/documents.js
+++ data/documents.js
@@ -1,5 +1,135 @@
 const documents = [
 
+
+    {
+    id: 1787020326180,
+
+    title: "Guidelines for sports activities and utilization of funds allotted under the scheme \"Promotion of Sports & Games for Block level Competition, equipment and other facilities\" in SSD Schools",
+
+    letterNo: "--",
+
+    date: "0001-01-01",
+
+    category: "Administration",
+
+    type: "Guidelines",
+
+    authority: "ST & SC Development Department, Government of Odisha",
+
+    summary: "Detailed guideline for utilization of sports grant in SSD schools including KMRS/BPAV.",
+
+    keywords: [
+        "sports grant"
+    ],
+
+    important: false,
+
+    file: "documents/Sports-grant-usage-guidelines.pdf"
+},
+  {
+    id: 1787019996659,
+
+    title: "Guidelines for undertaking minor repair and maintenance works of residential schools and hostels",
+
+    letterNo: "772/SSD",
+
+    date: "2023-01-17",
+
+    category: "Administration",
+
+    type: "Guidelines",
+
+    authority: "ST & SC Development Department, Government of Odisha",
+
+    summary: "Detailed guideline/SOP for undertaking minor repair and maintenance works of residential schools and hostels",
+
+    keywords: [
+        "Repair and Maintenance Guidelines"
+    ],
+
+    important: false,
+
+    file: "documents/SOP-Repair & Maintainance.pdf"
+},
+    {
+    id: 1787019848155,
+
+    title: "Guidelines on the implementation of School Cabinet Programme in all SSD Schools",
+
+    letterNo: "653/CSSD",
+
+    date: "2012-08-22",
+
+    category: "Administration",
+
+    type: "Guidelines",
+
+    authority: "ST & SC Development Department, Government of Odisha",
+
+    summary: "Detailed guideline on the implementation of School Cabinet Programme in all SSD Schools",
+
+    keywords: [
+        "School Cabinet",
+        "Guidelines"
+    ],
+
+    important: false,
+
+    file: "documents/School_Cabinet_22082012.pdf"
+},
+    {
+    id: 1787019658232,
+
+    title: "SOP for installation and monitoring of complaints received in \"Complaint Box\" in all SSD Hostels",
+
+    letterNo: "33963---",
+
+    date: "2014-12-24",
+
+    category: "Hostel",
+
+    type: "SOP",
+
+    authority: "ST & SC Development, M & BCW Department, Government of Odisha",
+
+    summary: "Detailed guideline/SOP on the installation and monitoring of complaints received in \"Complaint Box\" in all SSD Hostels.",
+
+    keywords: [
+        "Compliant Box",
+        "SOP Complaint Box"
+    ],
+
+    important: false,
+
+    file: "documents/Complaint Box SOP.pdf"
+},
+  {
+    id: 1787019463761,
+
+    title: "Guidelines for constitution and functioning of Sexual Harassment Committee in SSD schools",
+
+    letterNo: "33963",
+
+    date: "2014-12-24",
+
+    category: "Hostel",
+
+    type: "Guidelines",
+
+    authority: "ST & SC Development, M & BCW Department, Government of Odisha",
+
+    summary: "Detailed guideline/SOP on the constitution and functioning of Sexual Harassment Committee in SSD schools",
+
+    keywords: [
+        "POCSO Box",
+        "SOP POCSO Box",
+        "Sexual Harassment Committee"
+    ],
+
+    important: false,
+
+    file: "documents/POCSO SOP.pdf"
+},
     {
         id: 1786326911516,
 
